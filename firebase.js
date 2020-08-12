371c10078af86e037c36be83f8f2cca9b69d9fed
Nirav Bavishi <niravbavishi007@gmail.com>
2020-08-12 15:57:05 -0400

    Initial Commit Project has started

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBABoPTgdxFyfjGxWwtn4aseUEoECIAMS0",
+  authDomain: "tik-tok-clone-fad9b.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-fad9b.firebaseio.com",
+  projectId: "tik-tok-clone-fad9b",
+  storageBucket: "tik-tok-clone-fad9b.appspot.com",
+  messagingSenderId: "882930336361",
+  appId: "1:882930336361:web:68b94549e40d6606eb58f8",
+  measurementId: "G-G9HL60J9N7"
+};
