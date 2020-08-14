9499874dd8baa4ab0df072b5d93e8681f1e0ebac
Nirav Bavishi <niravbavishi007@gmail.com>
2020-08-14 17:17:24 -0400

    Need to fix Footer CSS and working on Video Footer

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./VideoFooter.css";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import Ticker from "react-ticker";
 
 function VideoFooter() {
   return (
     <div className="videoFooter">
-      <h2>I am a footer</h2>
+      <div className="videoFooter__text">
+        <h3>@niravbavishi</h3>
+        <p>This is For description</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+          <Ticker mode="smooth">
+            {({ index }) => <p>This is my ticker</p>}
+          </Ticker>
+        </div>
+      </div>
+      <img
+        className="videoFooter__record"
+        src="https://static.thenounproject.com/png/934821-200.png"
+        alt=""
+      ></img>
     </div>
   );
 }
